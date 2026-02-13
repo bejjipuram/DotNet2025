752ba48ee0aca3076e366b6ca2946790dfe1dcee
INDRA KUMAR BEJJIPURAM <tm88indrakumar2020@gmail.com>
2026-02-13 16:30:16 +0530

    Day 42 Code Files are uploaded

---
+++ Day 42 M1Practice/SmartBank/BusinessLogicClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CAP2025.Day_42_M1Practice.SmartBank
+{
+    public class CreditRiskProcessor
+    {
+        // Validation Method
+        public static bool ValidateCustomerDetails(
+            int age,
+            string employmentType,
+            double monthlyIncome,
+            double dues,
+            int creditScore,
+            int defaults)
+        {
+            if (age < 21 || age > 65)
+            {
+                Console.WriteLine("Invalid age");
+            }
+
+            if (employmentType != "Salaried" && employmentType != "Self-Employed")
+            {
+                Console.WriteLine("Invalid Employement Type");
+            }
+
+            if (monthlyIncome < 20000)
+            {
+                Console.WriteLine("Invalid monthly income");
+            }
+
+            if (dues < 0)
+            {
+                Console.WriteLine("Invalid credit dues");
+            }
+
+            if (creditScore < 300 || creditScore > 900)
+            {
+                Console.WriteLine("Invalid credit score");
+            }
+
+            if (defaults < 0)
+            {
+                Console.WriteLine("Invalid default count");
+            }
+
+            return true;
+        }
+
+        // Credit Limit Calculation
+        public static double CalculateCreditLimit(
+            double monthlyIncome,
+            double dues,
+            int creditScore,
+            int defaults)
+        {
+            double debtRatio = dues / (monthlyIncome * 12);
+
+            // High Risk
+            if (creditScore < 600 || defaults >= 3 || debtRatio > 0.4)
+            {
+                return 50000;
+            }
+
+            // Low Risk
+            if (creditScore >= 750 && defaults == 0 && debtRatio < 0.25)
+            {
+                return 300000;
+            }
+
+            // Medium Risk
+            return 150000;
+        }
+    }
+}
---
+++ Day 42 M1Practice/SmartBank/MainClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAP2025.Day_42_M1Practice.SmartBank
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter customer name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter employment type: ");
+                string employmentType = Console.ReadLine();
+
+                Console.Write("Enter monthly income: ");
+                double monthlyIncome = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter existing credit dues: ");
+                double dues = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter credit score: ");
+                int creditScore = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number of loan defaults: ");
+                int defaults = int.Parse(Console.ReadLine());
+
+                // Validate
+                if (CreditRiskProcessor.ValidateCustomerDetails(
+                        age,
+                        employmentType,
+                        monthlyIncome,
+                        dues,
+                        creditScore,
+                        defaults))
+                {
+                    double creditLimit = CreditRiskProcessor.CalculateCreditLimit(
+                        monthlyIncome,
+                        dues,
+                        creditScore,
+                        defaults);
+
+                    Console.WriteLine($"Customer Name: {name} Approved Credit Limit: {creditLimit}");
+                }
+            }
+            catch (InvalidCreditException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
