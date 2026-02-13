1d83ec2eeab3ebd16698a84aa7db0d0235601f39
INDRA KUMAR BEJJIPURAM <tm88indrakumar2020@gmail.com>
2026-02-13 16:35:52 +0530

    Day 42 Code Files are uploaded

---
+++ Day 42 M1Practice/SqlADO2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAP2025.Day_42_M1Practice
+{
+   public class SqlMain
+    {
+        public static void Main()
+        {
+            string cs = "Data Source=INDRA-S-LAPII\\SQLEXPRESS;Initial Catalog=NormalizationDemo;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;";
+            string sql = "SELECT CustomerId, CustomerName, CustomerPhone, CustomerCity FROM dbo.Customer ORDER BY CustomerId";
+
+            using (var con = new SqlConnection(cs))
+            using (var cmd = new SqlCommand(sql, con))
+            {
+                con.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                        string phone = reader.GetInt32(2);
+                        decimal city = reader.GetString(3);
+
+                        Console.WriteLine($"{id} | {name} | {phone} | {city}");
+                    }
+                }
+            }
+        }
+    }
+}
